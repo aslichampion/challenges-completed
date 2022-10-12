bf55279cf568ef0a8da84c275bbe50ad87aa3073
Asli Champion <30733811+aslichampion@users.noreply.github.com>
2022-10-12 10:54:15 +0100

    updated test files

---
+++ golden_square_skill_projects/spec/letter_counter_spec.rb
@@ -0,0 +1,19 @@
+include 'letter_counter'
+
+RSpec.describe 'letter_counter method' do
+    it "counts the most common character" do
+        
+    end
+
+    it "returns the character's position in the initial string" do
+        
+    end
+
+
+
+
+RSpec.describe 'arrays and hashes' do
+    describe 'first_element' do
+      it 'given [1,2,3], returns 1' do
+        expect(first_element([1,2,3])).to eq 1
+      end
